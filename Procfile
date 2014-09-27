f731709bb1390434ca8628bba7f52a2a30fa6649
Chris Sims <chris@jcsi.ms>
2014-09-26 20:47:10 -0700

    Let's try this again.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/qualityclj.jar clojure.main -m qualityclj.server
---
+++ src/clj/qualityclj/server.clj
@@ -0,0 +1,46 @@
+(ns qualityclj.server
+  (:require [clojure.java.io :as io]
+            [compojure.core :refer [GET defroutes]]
+            [compojure.route :refer [resources]]
+            [compojure.handler :refer [site]]
+            [net.cgrand.enlive-html :as html :refer [deftemplate]]
+            [environ.core :refer [env]]
+            [cemerick.piggieback :as piggieback]
+            [weasel.repl.websocket :as weasel]
+            [ring.middleware.reload :as reload]
+            [ring.adapter.jetty :refer [run-jetty]])
+  (:gen-class))
+
+(def is-dev? (env :is-dev))
+
+(defn body-transforms []
+  (if is-dev?
+    (comp
+     (html/set-attr :class "is-dev")
+     (html/prepend (html/html [:script {:type "text/javascript" :src "/out/goog/base.js"}]))
+     (html/prepend (html/html [:script {:type "text/javascript" :src "/react/react.js"}]))
+     (html/append  (html/html [:script {:type "text/javascript"} "goog.require('qualityclj.core')"])))
+    identity))
+
+(deftemplate page
+  (io/resource "index.html") [] [:body] (body-transforms))
+
+(defroutes routes
+  (resources "/")
+  (resources "/react" {:root "react"})
+  (GET "/*" req (page)))
+
+(defn browser-repl []
+  (piggieback/cljs-repl :repl-env (weasel/repl-env :ip "0.0.0.0" :port 9001)))
+
+(defn run [& [port]]
+  (defonce ^:private server
+    (let [handler (if is-dev?
+                    (reload/wrap-reload (site #'routes))
+                    (site routes))]
+      (run-jetty handler {:port (Integer. (or port (env :port) 10555))
+                                  :join? false})))
+  server)
+
+(defn -main [& [port]]
+  (run port))
