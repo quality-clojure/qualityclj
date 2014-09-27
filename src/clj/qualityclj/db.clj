8bc310eebb8668247bd6da2324878b431e44e673
Chris Sims <chris@jcsi.ms>
2014-09-27 08:29:20 -0700

    Fix schema, db creation order.

--- src/clj/qualityclj/db.clj
+++ src/clj/qualityclj/db.clj
@@ -2,16 +2,17 @@
   (:require [datomic.api :as d]
             [clojure.java.io :as io]))
 
-(def uri (str "datomic:mem://quality-clj"))
+(def uri (str "datomic:mem://qualityclj"))
 (def schema-tx (read-string (slurp (io/resource "data/schema.edn"))))
 (def fixtures (read-string (slurp (io/resource "data/initial.edn"))))
 
 
-(def conn (atom nil))
+(defonce conn (atom nil))
 
 (defn ensure-db []
-  (reset! conn (d/connect uri))
-  (when (d/create-database uri)
-    (do
-      @(d/transact @conn schema-tx)
-      @(d/transact @conn fixtures))))
+  (let [new? (d/create-database uri)]
+    (reset! conn (d/connect uri))
+    (when new?
+      (do
+        @(d/transact @conn schema-tx)
+        @(d/transact @conn fixtures)))))
