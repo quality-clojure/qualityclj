54c1069a2a28b119492a9ce9c4037e0e797a02e2
Chris Sims <chris@jcsi.ms>
2014-09-28 09:31:05 -0700

    Example Datomic queries.

--- src/clj/qualityclj/models/db.clj
+++ src/clj/qualityclj/models/db.clj
@@ -1,5 +1,5 @@
 (ns qualityclj.models.db
-  (:require [datomic.api :as d]
+  (:require [datomic.api :as d :refer [q db]]
             [clojure.java.io :as io]))
 
 (def uri (str "datomic:free://localhost:4334/qualityclj"))
@@ -16,3 +16,28 @@
         @(d/transact @conn schema-tx)
         @(d/transact @conn fixtures)))))
 
+(defn get-repos
+  "Get a list of all repos in the database."
+  []
+  (map first (q '[:find ?repo
+                  :where [_ :repo/uri ?repo]]
+                (db @conn))))
+
+(defn get-repo
+  "Given the name of a repo, return a database entity representing the
+  repository."
+  [name]
+  (let [id (q '[:find ?repo
+                :in $ ?name
+                :where
+                [?repo :repo/uri ?uri]
+                [(.contains ^String ?uri ^String ?name)]]
+              (db @conn) name)]
+    (d/entity (db @conn) (ffirst id))))
+
+(defn get-files
+  
+  [repo]
+  (q '[:find ?file
+       :in $ ?repo
+       :where []]))
