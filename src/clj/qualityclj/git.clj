2d864af7c5f443bf6f1555a3de0a6efa6ad11bc5
Scott Bauer <sbauer322@gmail.com>
2014-10-04 23:56:37 -0400

    Added functionality to clone a git repo into a local directory.
    Introduced clj-jgit.

---
+++ src/clj/qualityclj/git.clj
@@ -0,0 +1,9 @@
+(ns qualityclj.git
+  (:require [clj-jgit.porcelain :as git]))
+
+(defn import-repo 
+  "Given a correctly formed url for a git repo, the function will clone and pull
+   the repo to the specified local directory. Local directory must be empty, or 
+   else."
+  [repo-url repo-dir]
+  (git/git-clone-full repo-url (str "repos/" repo-dir)))
