8c0aa042f13e23ad2f5d9d8b110a5853b2111baf
Chris Sims <chris@jcsi.ms>
2014-10-06 20:13:13 -0700

    Replace `get-rel-path`.
    `clojure.string.replace-first` does the same thing.

--- src/clj/qualityclj/imports/highlight.clj
+++ src/clj/qualityclj/imports/highlight.clj
@@ -6,18 +6,6 @@
 (def repo-path "repos")
 (def highlight-path "highlight")
 
-(defn get-rel-path
-  "Given a full path and a base path, return the path specific to the
-  full path.
-
-  Example:
-  (get-rel-path \"repos/jcsims/cljagents/src/clj/cljagents/agent.clj\"
-  \"repos\")
-  => \"jcsims/cljagents/src/clj/cljagents/agent.clj\""
-  [full-path base]
-  (let [base (if (.endsWith base "/") base (str base "/"))]
-    (s/replace-first full-path base "")))
-
 (defn highlight
   "Produce an HTML file from a given source file. Takes a full path to
   the original source file as well as an output file path."
@@ -38,9 +26,11 @@
         out-path (io/file (str highlight-path "/" project "/"))]
     (when-not (.exists out-path) (.mkdirs out-path))
     (doseq [file (file-seq src-path)]
-      (when (.isFile file)
-        (let [out-file (io/file (str highlight-path "/"
-                                     (get-rel-path (.getPath file) repo-path)
+      (when (and (.isFile file) (.endsWith (.getPath file) "clj"))
+        (let [out-file (io/file (str (s/replace-first
+                                      (.getPath file)
+                                      repo-path
+                                      highlight-path)
                                      ".html"))]
           (.mkdirs (.getParentFile out-file))
           (highlight (.getPath file) (.getPath out-file)))))))
