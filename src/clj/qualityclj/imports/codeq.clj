3b959cdbcd326027f3ab2227a3bc1b27fa060c5c
Scott Bauer <sbauer322@gmail.com>
2014-10-08 00:17:57 -0400

    Removed clygments.
    Changed absolute path to path.

--- src/clj/qualityclj/imports/codeq.clj
+++ src/clj/qualityclj/imports/codeq.clj
@@ -2,7 +2,6 @@
   (:require [qualityclj.models.db :refer [get-repo]])
   (:import java.io.File))
 
-
 (def uri "datomic:free://localhost:4334/qualityclj")
 (def repo-path "repos")
 (def codeq-path "../../../codeq-0.1.0-SNAPSHOT-standalone.jar")
@@ -11,10 +10,10 @@
   "Feed the codeq from the local repository directory."
   [user repo]
   (let [codeq-filepath (.getPath (clojure.java.io/file codeq-path))
-        repo-filepath (.getAbsolutePath (clojure.java.io/file 
-                                          (str repo-path 
-                                            File/separator user 
-                                            File/separator repo)))]
+        repo-filepath (.getPath (clojure.java.io/file 
+                                  (str repo-path 
+                                    File/separator user 
+                                    File/separator repo)))]
     (when (nil? (get-repo user repo))
       (clojure.java.shell/sh "java" "-server" "-Xmx1g" "-jar" 
         codeq-filepath uri :dir repo-filepath))))
