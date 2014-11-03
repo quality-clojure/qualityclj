3f7472bd44ac62f66f32b86cb77cf8a633ae61fa
Chris Sims <chris@jcsi.ms>
2014-11-03 10:06:27 -0800

    Remove invalid test.

--- test/clj/qualityclj/routes/home_test.clj
+++ test/clj/qualityclj/routes/home_test.clj
@@ -2,6 +2,3 @@
   (:require [qualityclj.models.db :as db])
   (:use qualityclj.routes.home
         clojure.test))
-
-(deftest proper-git-url
-  (is (thrown? IllegalArgumentException (import-project "hello"))))
